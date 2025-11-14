b287d584621c0fb4f184d281b8bdf7413c69d1b3
Mirco Kraenz <software@kraenz.eu>
2025-11-14 14:42:39 +0100

    init

---
+++ src/main/java/eu/kraenz/moshstore/MoshstoreApplication.java
@@ -0,0 +1,13 @@
+package eu.kraenz.moshstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoshstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoshstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/kraenz/moshstore/MoshstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kraenz.moshstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoshstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
